866b7ca8ca6f13b237a1ad42bfebcf846a23bd34
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-10-06 21:15:00 -0500

    updated Manager.test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,6 +1,17 @@
 const Manager = require("../lib/Manager");
 
 describe ("Manager", () =>{
+    describe("Initialization", () => {
+        it("should return an manager object", () => {
+            const manager = new Manager('Maria',1,'maria@fakemail.com',2135602580);
+
+            expect(manager.name).toEqual(expect.any(String));
+            expect(manager.id).toEqual(expect.any(Number));
+            expect(manager.email).toEqual(expect.any(String));
+            expect(manager.officeNumber).toEqual(expect.any(Number));
+        })
+    })
+
     describe("getOfficeNumber()", () => {
         it("should return an Manager office number", () => {
             const officeNumber = 2135602580;
@@ -9,10 +20,11 @@ describe ("Manager", () =>{
             expect(manager.getOfficeNumber()).toBe(officeNumber);
         })
     })
+
     describe("getRole()", () => {
         it("should return a manager role", () => {
             const role = 'Manager';
-            const manager = new Manager('Maria',1,'maria@fakemail.com');
+            const manager = new Manager('Maria',1,'maria@fakemail.com',2135602580);
 
             expect(manager.getRole()).toBe(role);
         })
